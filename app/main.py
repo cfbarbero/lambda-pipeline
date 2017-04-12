04f7ccd74b11e020d76e1cc4ce5f4e361fe20ab5
crisb <cfbarbero@gmail.com>
2017-04-12 12:46:14 -0600

    look at container cycling

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 import datetime
+import os
+import time
+
+was = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
 
 
 def handler(event, context):
     now = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
-    print(now)
-    print("foo")
-    return now
+
+    time.sleep(5)
+
+    return was
